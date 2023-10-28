875f5dd8244a5bc00b4afdc5f78a98037d4a3d08
Martin <martin_rot@hotmail.com>
2023-10-28 18:09:53 -0300

    5

--- app/hooks/getData.jsx
+++ app/hooks/getData.jsx
@@ -5,13 +5,12 @@ export async function getData({ coleccion }) {
 
   let q = "";
   q = query(collection(db, coleccion));
-  const data = await getDocs(q);
+  const data = await fetch( getDocs(q), {next:{ revalidate:20 }} )
   const posts = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
   return {
     props: {
       posts,
-    },
-      revalidate: 30,
+    }
   };
 } 
